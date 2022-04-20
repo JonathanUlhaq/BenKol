a5b87d560431a2e7c2c3c5fa758e4ba1d04c3890
Jovian Nathan <vianulhag@gmail.com>
2022-04-20 22:39:36 +0700

    Layout OTP

--- app/src/main/java/com/example/benkol/Login.java
+++ app/src/main/java/com/example/benkol/Login.java
@@ -2,7 +2,9 @@ package com.example.benkol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void OTP(View view)
+    {
+        Intent intent = new Intent(Login.this,Otp.class);
+        startActivity(intent);
     }
 }
