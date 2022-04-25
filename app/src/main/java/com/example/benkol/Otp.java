a6a4c1b0d2ce6023872d3953fe9d3406e17e4639
Jovian Nathan <vianulhag@gmail.com>
2022-04-25 22:11:11 +0700

    Konfirmasi Screen

--- app/src/main/java/com/example/benkol/Otp.java
+++ app/src/main/java/com/example/benkol/Otp.java
@@ -2,14 +2,19 @@ package com.example.benkol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Otp extends AppCompatActivity {
-
-    TextView kirim;
+    private static final int SPLASH_TIME =  2*1000;
+    TextView kirim,login,konfirmasi,deskripsi;
+    ImageView simbol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +25,36 @@ public class Otp extends AppCompatActivity {
         kirim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Otp.this,"OTP berhasil dikirim",Toast.LENGTH_SHORT);
+                SendBack();
             }
         });
+    }
+
+    @SuppressLint({"NewApi", "Range"})
+    public void SendBack ()
+    {
+        setContentView(R.layout.acitivity_konfirmasi);
+        konfirmasi = findViewById(R.id.konfirmasi);
+        deskripsi = findViewById(R.id.deskripsi);
+        simbol = findViewById(R.id.simbol);
+        konfirmasi.setText("Registrasi Berhasil !");
+        deskripsi.setText("Yuk langsung aja login");
+        simbol.setImageResource(R.drawable.ic_sms_check);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Otp.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            }
+        }, SPLASH_TIME);
+
+
     }
 }
--- app/src/main/java/com/example/benkol/Register.java
+++ app/src/main/java/com/example/benkol/Register.java
@@ -1,22 +1,40 @@
 package com.example.benkol;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
-    TextView login;
+public class Register extends AppCompatActivity {
+    private static final int SPLASH_TIME =  2*1000;
+    TextView login,konfirmasi,deskripsi;
+    MaterialButton register;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+Konfirmasi();
+            }
+        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,12 +46,31 @@ public class Register extends AppCompatActivity {
     }
 
 
-    public void Register(View view)
-    {
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
 
-        Toast.makeText(this,"Register berhasil",Toast.LENGTH_SHORT);
+    @SuppressLint({"NewApi", "Range"})
+    public void Konfirmasi ()
+    {
+        setContentView(R.layout.acitivity_konfirmasi);
+        konfirmasi = findViewById(R.id.konfirmasi);
+        deskripsi = findViewById(R.id.deskripsi);
+        konfirmasi.setText("Registrasi Berhasil !");
+        deskripsi.setText("Yuk langsung aja login");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Register.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            }
+        }, SPLASH_TIME);
+
+
     }
 
 }
