4eabe82d3a7c89bad1daf9df0c1de42e6f7542c3
RamdhanSyaifulloh <56957725+ramdhansy12@users.noreply.github.com>
2022-04-25 15:56:18 +0700

    Penambahan Register

---
+++ app/src/main/java/com/example/benkol/Register.java
@@ -0,0 +1,20 @@
+package com.example.benkol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    private void setContentView(int activity_register) {
+
+    }
+
+}
