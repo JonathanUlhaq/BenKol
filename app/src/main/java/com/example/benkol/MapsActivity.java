23e869b62ff0fe272526ce0fc3ff79cbd78892ab
Jovian Nathan <vianulhag@gmail.com>
2022-04-29 13:13:13 +0700

    Tambahan Maps

---
+++ app/src/main/java/com/example/benkol/MapsActivity.java
@@ -0,0 +1,328 @@
+package com.example.benkol;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.benkol.MapsActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    LocationManager locationManagers;
+    LocationRequest mLocationRequest;
+    Button jasa,jam;
+    TextView tittle,status;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+
+
+
+        mMap.setOnMarkerClickListener(this);
+
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+        LatLng bengkel3 = new LatLng(-7.654, 111.333);
+        googleMap.addMarker(new MarkerOptions()
+                .position(bengkel3)
+                .title("Bengkel Tesla")
+                .flat(false));
+
+        LatLng bengkel2 = new LatLng(-7.654, 111.334);
+        googleMap.addMarker(new MarkerOptions()
+                .position(bengkel2)
+                .title("Bengkel Motor Racing")
+                .flat(true));
+
+        LatLng bengkel1 = new LatLng(-7.654, 111.332);
+        googleMap.addMarker(new MarkerOptions()
+                .position(bengkel1)
+                .title("Bengkel Abadi Amanah")
+        .flat(false));
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+
+
+    @SuppressLint({"MissingPermission", "RestrictedApi"})
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+//Showing Current Location Marker on Map
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        @SuppressLint("MissingPermission") Location locations = locationManager.getLastKnownLocation(provider);
+        List<String> providerList = locationManager.getAllProviders();
+        if (null != locations && null != providerList && providerList.size() > 0) {
+            double longitude = locations.getLongitude();
+            double latitude = locations.getLatitude();
+            Geocoder geocoder = new Geocoder(getApplicationContext(),
+                    Locale.getDefault());
+            try {
+                List<Address> listAddresses = geocoder.getFromLocation(latitude,
+                        longitude, 1);
+                if (null != listAddresses && listAddresses.size() > 0) {
+                    String state = listAddresses.get(0).getAdminArea();
+                    String country = listAddresses.get(0).getCountryName();
+                    String subLocality = listAddresses.get(0).getSubLocality();
+                    markerOptions.title("" + latLng + "," + subLocality + "," + state
+                            + "," + country);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,
+                    this);
+        }
+
+    }
+
+    @SuppressLint({"MissingPermission", "MissingSuperCall"})
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied",
+                            Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Toast.makeText(this, "Udah ter klik guys", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                this,R.style.BottomSheetDialogTheme
+        );
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(
+                R.layout.activity_bottom, (LinearLayout) findViewById(R.id.info_bawah)
+        );
+        tittle = bottomSheetView.findViewById(R.id.tittle);
+        tittle.setText(marker.getTitle());
+
+
+        status = bottomSheetView.findViewById(R.id.status);
+        jasa = bottomSheetView.findViewById(R.id.jassa);
+        jam = bottomSheetView.findViewById(R.id.jam);
+
+        jam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu dropDown = new PopupMenu(getApplicationContext(),jam);
+                dropDown.getMenuInflater().inflate(R.menu.item_pelayanan,dropDown.getMenu());
+
+                dropDown.show();
+            }
+        });
+
+        jasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu dropDown = new PopupMenu(getApplicationContext(),jasa);
+                dropDown.getMenuInflater().inflate(R.menu.item_jam,dropDown.getMenu());
+
+                dropDown.show();
+            }
+        });
+
+        if(marker.isFlat())
+        {
+            status.setText("Buka");
+            status.setTextColor(getResources().getColor(R.color.kuning));
+        }
+         else {
+            status.setText("Tutup");
+            status.setTextColor(getResources().getColor(R.color.merah));
+        }
+
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+        return false;
+    }
+}
--- app/src/main/java/com/example/benkol/Otp.java
+++ app/src/main/java/com/example/benkol/Otp.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,12 +16,21 @@ public class Otp extends AppCompatActivity {
     private static final int SPLASH_TIME =  2*1000;
     TextView kirim,login,konfirmasi,deskripsi;
     ImageView simbol;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
         kirim = findViewById(R.id.kirim);
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                berhasil();
+            }
+        });
 
         kirim.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,20 +41,48 @@ public class Otp extends AppCompatActivity {
     }
 
     @SuppressLint({"NewApi", "Range"})
+    public void berhasil ()
+    {
+        setContentView(R.layout.acitivity_konfirmasi);
+        konfirmasi = findViewById(R.id.konfirmasi);
+        deskripsi = findViewById(R.id.deskripsi);
+        simbol = findViewById(R.id.simbol);
+        konfirmasi.setText("Berhasil Login");
+        deskripsi.setText("Temukan Bengkelmu");
+        simbol.setImageResource(R.drawable.ic_konfirmasi);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Otp.this,MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            }
+        }, SPLASH_TIME);
+
+
+    }
+
+    @SuppressLint({"NewApi", "Range"})
     public void SendBack ()
     {
         setContentView(R.layout.acitivity_konfirmasi);
         konfirmasi = findViewById(R.id.konfirmasi);
         deskripsi = findViewById(R.id.deskripsi);
         simbol = findViewById(R.id.simbol);
-        konfirmasi.setText("Pesan Berhasil dikirim Ulang");
+        konfirmasi.setText("Berhasil Resend");
         deskripsi.setText("Cek Pesan Kamu");
         simbol.setImageResource(R.drawable.ic_sms_check);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Otp.this,Login.class);
+                Intent intent = new Intent(Otp.this,Otp.class);
                 startActivity(intent);
                 finish();
             }
